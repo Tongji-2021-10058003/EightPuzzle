13b85a90f519b4b77a098274d85ae1ce7fda3f9a
true_mogician <925269713@qq.com>
2021-04-20 23:26:06 +0800

    chore(project): create EightPuzzle project

---
+++ EightPuzzle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzle {
+	public class Program {
+		class Foo : IComparable<Foo> {
+			public int Value;
+			public Foo(int value = default) => Value = value;
+			public int CompareTo(Foo other) {
+				Console.WriteLine($"Comparing {Value} and {other.Value}");
+				return Value.CompareTo(other.Value);
+			}
+		}
+		static void Main() {
+			var heap = new C5.IntervalHeap<Foo>();
+			var foos = new Foo[] { new Foo(0), new Foo(1), new Foo(2) };
+			foreach (var foo in foos)
+				heap.Add(foo);
+		}
+	}
+}
