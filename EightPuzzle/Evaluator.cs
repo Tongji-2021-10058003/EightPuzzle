68d54c08b2e2b63304cb3c2743f9b94bb015882d
true_mogician <925269713@qq.com>
2021-04-20 22:23:31 +0800

    feat(Evaluator): predefined evaluators

---
+++ EightPuzzle/Evaluator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EightPuzzle {
+	public static class Evaluator {
+		public static readonly Search<Puzzle>.Evaluator ManhattanDistance =
+			(src, dst) => Math.Abs(src.Slot.Row - dst.Slot.Row) + Math.Abs(src.Slot.Column - dst.Slot.Column);
+	}
+}
