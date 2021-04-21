7f5aec3269b4cfc587157940cc495fc539cbaa2e
true_mogician <925269713@qq.com>
2021-04-21 12:03:28 +0800

    fix(Evaluator): compile error

--- EightPuzzle/Evaluator.cs
+++ EightPuzzle/Evaluator.cs
@@ -2,16 +2,17 @@ using System;
 
 namespace EightPuzzle {
 	public static class Evaluator {
+		private static int Square(int value) => value * value;
 		public static readonly Search<Puzzle>.Evaluator ManhattanDistance =
 			(src, dst) => Math.Abs(src.Slot.Row - dst.Slot.Row) + Math.Abs(src.Slot.Column - dst.Slot.Column);
-			
+
 		public static readonly Search<Puzzle>.Evaluator EuclideanDistance =
-			(src, dst) => Math.sqrt(Math.pow((src.Slot.Row - dst.Slot.Row), 2) + Math.pow((src.Slot.Column - dst.Slot.Column), 2));
-			
+			(src, dst) => (int)Math.Round(Math.Sqrt(Square(src.Slot.Row - dst.Slot.Row) + Square(src.Slot.Column - dst.Slot.Column)));
+
 		public static readonly Search<Puzzle>.Evaluator MalposedNumDistance =
-			(src, dst) => ((Math.Abs(src.Slot.Row - dst.Slot.Row) > 0)||(Math.Abs(src.Slot.Column - dst.Slot.Column) > 0)) ? 1 : 0;
-			
+			(src, dst) => (Math.Abs(src.Slot.Row - dst.Slot.Row) > 0 || Math.Abs(src.Slot.Column - dst.Slot.Column) > 0) ? 1 : 0;
+
 		public static readonly Search<Puzzle>.Evaluator DiagonalDistance =
-			(src, dst) =>  ((Math.Abs(src.Slot.Row - dst.Slot.Row) == 2) ? 2 : ((Math.Abs(src.Slot.Row - dst.Slot.Row) == 0) ? Math.Abs(src.Slot.Column - dst.Slot.Column) : ((Math.Abs(src.Slot.Column - dst.Slot.Column) == 0) ? 1 : Math.Abs(src.Slot.Column - dst.Slot.Column))));
+			(src, dst) => ((Math.Abs(src.Slot.Row - dst.Slot.Row) == 2) ? 2 : ((Math.Abs(src.Slot.Row - dst.Slot.Row) == 0) ? Math.Abs(src.Slot.Column - dst.Slot.Column) : ((Math.Abs(src.Slot.Column - dst.Slot.Column) == 0) ? 1 : Math.Abs(src.Slot.Column - dst.Slot.Column))));
 	}
 }
